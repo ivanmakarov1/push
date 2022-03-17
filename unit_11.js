e48355462a9df9f5c6ef83f9902294a13b9cd5e9
ivanmakarov1 <ivanmakarov1@yandex.ru>
2022-03-17 14:26:37 +0300

    ртр67

--- unit_11.js
+++ unit_11.js
@@ -229,7 +229,7 @@ function f14() {
   let d14 = [];
   let val = +document.querySelector(".i-14").value;
   for (let i = 0; i < val; i++) {
-    d14[d14.length] = 1;
+    d14.push(1);
   }
 
   showArr(".out-14", d14);
